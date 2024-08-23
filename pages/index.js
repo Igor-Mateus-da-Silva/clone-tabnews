f3a28fd4b8266a9e35d75909ef4762fc534fbafb
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-08-23 10:43:19 +0000

    index page link to `API`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
-function Home() {
-  return <h1>Hello World</h1>;
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.text}>Consultar API</h1>
+      <Link href="/api/v1/status">
+        <span style={styles.button}>Ir para API</span>
+      </Link>
+    </div>
+  );
 }
 
-export default Home;
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "98vh",
+    backgroundColor: "#2d2d2d", // Tom de grafite escuro
+  },
+  text: {
+    color: "#ffffff", // Cor branca para o texto
+    fontSize: "2rem",
+    marginBottom: "20px",
+  },
+  button: {
+    padding: "10px 20px",
+    backgroundColor: "#1e40af",
+    color: "#ffffff",
+    textDecoration: "none",
+    borderRadius: "5px",
+    transition: "background-color 0.3s",
+  },
+};
